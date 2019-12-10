1fd2748e058290d359489244e7cd49ae87ba7e40
CarlosVGC <awesomethebest@hotmail.es>
2019-12-09 21:30:20 -0600

    mi primer commit

---
+++ Hola_mundo.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec  9 20:43:58 2019
+
+@author: Thebl
+"""
+
+print("Texto a mostrar")
+cadena='hola mUndo'
+print(cadena.title()+ ' title() Imprime cada letra inicial de la cadena en M')
+print(cadena.lower())
+print(cadena.upper())
