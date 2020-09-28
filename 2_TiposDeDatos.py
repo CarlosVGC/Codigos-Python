7e6bad02b6f0d691baaf32445d8cc7c9a4c713cc
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-28 13:34:41 -0500

    Visualización simple de tipos de datos

---
+++ 2_TiposDeDatos.py
@@ -0,0 +1,26 @@
+#Tipos de datos en python
+
+numero = 89
+print(numero)
+print(type(numero))
+print('------------------------')
+numeroFlotante = 16.89
+print(numeroFlotante)
+print(type(numeroFlotante))
+print('------------------------')
+cadena = "Esto es una cadena"
+print(cadena)
+print(type(cadena))
+print('------------------------')
+boleano = False
+print(boleano)
+print(type(boleano))
+print('------------------------')
+hibridoentfloat = numero + numeroFlotante
+print(hibridoentfloat)
+print(type(hibridoentfloat)) 
+print('------------------------')
+cadena = hibridoentfloat
+print(cadena)
+print(type(cadena)) 
+
