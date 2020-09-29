d92e496006b1efa3ac0f3fd7d0713de8be88be6a
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-29 18:39:47 -0500

    Expresión Matematica  usando input

---
+++ 12_ExpresionMat2.py
@@ -0,0 +1,9 @@
+#Expresion matermatica 
+a = float(input("Ingresa el elemento a: "))
+b = float(input("Ingresa el elemento b: "))
+c = float(input("Ingresa el elemento c: "))
+d = float(input("Ingresa el elemento d: "))
+e = float(input("Ingresa el elemento e: "))
+f = float(input("Ingresa el elemento f: "))
+sol = (a+ (b/c))/(d+(e/f))
+print(f"La solución de la expresión es: {sol:,.3f}")
