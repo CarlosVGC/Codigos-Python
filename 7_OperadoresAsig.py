5d92ab6363d1cac34e8fc5ac63f09cffd332762a
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-29 16:28:21 -0500

    Operadores de asignación
    Se muestra el uso básico de los operadores de asignación

---
+++ 7_OperadoresAsig.py
@@ -0,0 +1,24 @@
+"""
+
+"""
+a=1
+b=1
+c=1
+d=1
+e=1
+f=1
+
+#a= a+1
+a+= 1
+print("a+= ", a)
+b-= 1
+print("b -= ", b)
+c*= 1
+print("c *= ", c)
+d/= 1
+print("d /=", d)
+e**= 1
+print("e **=", e)
+f%= 1
+print("f %=", f)
+
