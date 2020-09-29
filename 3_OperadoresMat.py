a5bbafaf1cbad5b4966d7ede2a806fc10e209ed0
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-28 22:51:30 -0500

    Uso de operadores Matematicos

---
+++ 3_OperadoresMat.py
@@ -0,0 +1,23 @@
+#Programa en el que se usan operadores matemáticos
+n1 = 10
+n2 = 2
+
+resultado = n1 + n2
+print("La suma es: ", resultado) # se concatena entero con coma (,)
+resultado = n1 - n2
+print("La resta es: ",  resultado)
+resultado = n1 * n2
+print("La multiplicacion es: ",  resultado)
+resultado = n1 / n2
+print("La division es: ",  resultado)
+resultado = n1 // n2
+print("La division entera es: ",  resultado)
+resultado = n1 % n2
+print("El modulo es: ",  resultado)
+resultado = n1 ** n2
+print("El exponente es: ",  resultado)
+
+#haciendo expresion matematica
+
+res = (3**3)*(13/5*(2*4))
+print("El resultado es: ", res)
