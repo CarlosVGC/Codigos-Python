77c1ef92bd90559a266343d0242b4a522fe7e463
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-29 16:57:21 -0500

    Imprimir textos en consola
    Se usa fformat asi como diferentes tipos de formatos para imprimir datos en la consola

---
+++ 8_SalidaConsola.py
@@ -0,0 +1,7 @@
+#Salida de consola 
+nombre = 'Carlos'
+edad = 26
+print ("Hola tu nombre es ", nombre ,"La edad que tienes es: ", edad)
+print ("Hola tu nombre es {0} y tu edad es {1:10.3f} adios {0}".format(nombre, edad)) 
+# Se le da un formato {0} es la variable que se encuenta al inicio(nombre), 10.3 son 10 espacios a la izq y 3 decimales despues del punto f= flotante
+print(f"Tu nombre es {nombre} edad {edad: 10.3f}")# f string es para dar formato a las variables
