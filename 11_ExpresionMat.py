57ca25f61fe3495f829f8aed71534ebdcf6b3ded
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-29 18:27:45 -0500

    Aplicación en la que se plasma una ecxpresion matemática
    Se usa format string asi como conversion de datos

---
+++ 11_ExpresionMat.py
@@ -0,0 +1,8 @@
+#Conversion matematica a codigo
+
+a = float(input ("Ingresa el valor de a: "))
+b = float(input ("Ingresa el valor de b: "))
+
+sol = (a/b) + 1
+print(f"El resultado de la expresion es: {sol:,.3f}")
+#coma para separar las cantidades, 3 son los decimales que se mostraran  y f es el tipo (flotante)
