2ffee4e206ebc5ff2983395840d58e7c5ec19fef
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-29 17:45:45 -0500

    Usando funcion input
    Realizando operaciones matematicas, usando la funcion input, se aplica la conversion de un cadena a un flotante

---
+++ 10_OperacionesAritmeticas.py
@@ -0,0 +1,11 @@
+# Programa donde se realizan diferentes tipos de operaciones
+
+n1 = float(input("Ingresa el primer numero: "))
+n2 = float(input("Ingresa el primer numero: "))
+
+suma = n1 + n2
+resta = n1 - n2
+mult = n1 * n2
+div = n1 / n2
+
+print(f"El resultado de las operaciones es:\nsuma:  {suma}\nresta: {resta}\nmult: {mult}\ndiv: {div: 1.3} ")
