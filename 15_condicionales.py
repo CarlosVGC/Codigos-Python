2ce66ea214e48ebb245cae13661c0e4212f09ab2
CarlosVGC <58715697+CarlosVGC@users.noreply.github.com>
2020-09-30 21:14:00 -0500

    Uso básico del if

---
+++ 15_condicionales.py
@@ -0,0 +1,10 @@
+#Uso básico de los condicionales
+
+numero = float(input("Ingresa un numero: "))
+
+if numero < 0:
+    print("Su numero ingresado es negativo")
+elif (numero == 0):
+    print("Su numero es cero")
+else:
+    print("Su numero es positivo")
