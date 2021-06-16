f75df4518f26a8dec1eafb24364f37705dad9f41
Carlos Venegas <awesomethebest@hotmail.es>
2021-06-15 20:50:52 -0500

    Primera Sesion

---
+++ Curso_Udemy/CursoUdemy/Seccion 2/1_Variables.py
@@ -0,0 +1,13 @@
+variable = "cadena" #almace cualquier tipo de informacion
+print(variable)
+print(variable)
+variable = "donde estas"
+print(variable)
+
+#-----------------
+x,y,z = 5,7,1 #asignar multiples valores a variables en una sola linea
+
+print(x+y+z)
+print(id(x)) #Se muestra la direccion de memoria donde se guarda la variable
+print(id(y))
+print(id(z)) #Referencias de memoria se le conoce donde se guardan los valores
---
+++ Curso_Udemy/CursoUdemy/Seccion 3/1_TiposDatos.py
@@ -0,0 +1,10 @@
+#Enteros: 1-9
+#Cadenas    "cadena" o 'cadena' se puede hacer con comillas simples o dobles
+#Flotantes: 1.1 -9.9
+#Booleanos: True False
+
+
+x: str = True # : str es un "hint" una pista a lo que apuntará la variable, se puede usar otro tipo de variable, solo es un indicador no define la variable
+print(x, end='') #se le quita el formato del salto de linea
+
+print(type(x)) #las variables apuntan a un tipo de memoria
