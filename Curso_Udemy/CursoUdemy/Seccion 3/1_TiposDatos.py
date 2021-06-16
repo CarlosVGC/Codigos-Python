3fba17a1ed63018f4038210551a035dc5f9a5813
Carlos Venegas <awesomethebest@hotmail.es>
2021-06-15 22:35:46 -0500

    Update 1_TiposDatos.py

--- Curso_Udemy/CursoUdemy/Seccion 3/1_TiposDatos.py
+++ Curso_Udemy/CursoUdemy/Seccion 3/1_TiposDatos.py
@@ -8,3 +8,13 @@ x: str = True # : str es un "hint" una pista a lo que apuntará la variable, se 
 print(x, end='') #se le quita el formato del salto de linea
 
 print(type(x)) #las variables apuntan a un tipo de memoria
+
+#manejo de cadenas
+grupofav = "Ace of Base"
+comentario = "La mejor agrupacion"
+print("Mi grupo favorito es: "+grupofav+ " "+ comentario) #se puede concatenar sin "+" si son cadenas consecutivas
+print("Mi grupo favorito es:", grupofav, comentario) #con la coma se agrega un espacio a la concatenacion
+print(f'Mi grupo favorito es: {grupofav} {comentario}') #otro tipo de formateo para impresion cadenas
+
+num1, num2 = 1, 2
+print(str(num1)+str(num2))
